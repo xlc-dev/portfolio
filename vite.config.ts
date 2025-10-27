a0f665c52a10c8969370e4058666e55cac162a3d
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2025-10-27 17:06:04 +0100

    github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [solid(), tailwindcss(), Icons({ compiler: "solid" })],
 });
